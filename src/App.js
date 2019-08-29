889eb40e2a65fd8553348e5f5a4f2ede7831b62a
raul samuel leos martinez <chs.desarolladores@gmail.com>
2019-08-29 18:19:20 -0500

    First commit, basic hamburger structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 //components
 import Layout from './components/Layout/Layout';
 import BurguerBuilder from './containers/BurguerBuilder/BurguerBuilder'
-
+import './App.module.css'
 function App() {
   return (
     <div>
--- src/components/Burguer/BurgerIngredients/BurgerIngredients.js
+++ src/components/Burguer/BurgerIngredients/BurgerIngredients.js
@@ -1,45 +1,43 @@
 import React, {Component} from 'react';
-import classes from './BurgerIngredient.css';
-import PropTypes from 'prop-types';
+import classes2 from './BurgerIngredient.css';
+// import PropTypes from 'prop-types';
 
 
 
 export default class BurgerIngredients extends Component {
     render(){
-        BurgerIngredients.propTypes = {
-            type: PropTypes.string.isRequired
-        }
         let ingredients = null;
 
+        console.log(this.props.type);
+
         switch(this.props.type) {
-            case ('bread-bottom'): 
-                ingredients = <div className={classes.BreadBottom}></div>;
+            case 'bread-bottom': 
+                ingredients = (<div className={classes2.BreadBottom}></div>);
                 break;
-            case ('bread-top'):
+            case 'bread-top':
                 ingredients = (
-                    <div className={classes.BreadTop}>
-                        <div className={classes.Seeds1}></div>
-                        <div className={classes.Seeds2}></div>
+                    <div className={classes2.BreadTop}>
+                        <div className={classes2.Seeds1}></div>
+                        <div className={classes2.Seeds2}></div>
                     </div>
                 );
                 break;
-            case ('meat'): 
-                ingredients = <div className={classes.Meat}></div>;
+            case 'meat': 
+                ingredients = <div className={classes2.Meat}></div>;
                 break;
-            case('cheese'):
-                ingredients = <div className={classes.Cheese}></div>;
+            case 'cheese':
+                ingredients = <div className={classes2.Cheese}></div>;
                 break;
-            case('salad'):
-                ingredients = <div className={classes.Salad}></div>;
+            case 'salad':
+                ingredients = <div className={classes2.Salad}></div>;
                 break;
-            case('bacon'):
-                ingredients = <div className={classes.Bacon}></div>;
+            case 'bacon':
+                ingredients = (<div className={classes2.Bacon}></div>);
                 break;
             default: 
                 ingredients = null;
                 break;
-    }
-
-    return ingredients;
+        }
+        return (ingredients);
     }
 }
